588f220f4ec6261e8b22beb2761bcdb74ce2211c
Frida Gutiérrez <fridagtzm@gmail.com>
2023-04-23 21:06:20 -0600

    Refactor class

--- utils/symbol_table.py
+++ utils/symbol_table.py
@@ -6,7 +6,7 @@ class SymbolTable:
         'programa': {
           'param_types': [],
           'return_type': 'void',
-          'kind': 'np', # np -> nombre de programa
+          'kind': 'np', # np -> nombre of program
           'variables': {
             'var_names': set(),
             'vars_info' : [],
@@ -16,7 +16,7 @@ class SymbolTable:
     }
 
   def add_variable(self, type, name, func_name, dimension=0, size=0):
-    """ Adds variables to variables table of the corresponding function """
+    """ Add variables to variables table of the corresponding function """
     access_dict = self.symbol_table["dir_functions"][func_name]["variables"]
     if name not in access_dict["var_names"]:
       access_dict["var_names"].add(name)
@@ -25,23 +25,24 @@ class SymbolTable:
       return ("ERROR: La variable {name} ya está declarada")
     
   def add_function(self, func_name, return_type): 
-    """ Adds functions to function directory if it doesn't exist """
+    """ Add functions to the function directory if it doesn't exist """
     access_dict = self.symbol_table["dir_functions"]["dir_func_names"]
     if func_name not in access_dict:
       access_dict.add(func_name)
       self.symbol_table["dir_functions"][func_name] = {
         'param_types': [],
+        'return_type': return_type,
+        'kind': 'function',
         'variables': {
           'var_names': set(),
           'vars_info' : [],
         },
       }
-      self.symbol_table["dir_functions"][func_name]["return_type"] = return_type
     else: 
-      return ("ERROR: La función {name} ya está declarada")
+      return ("ERROR: La función {func_name} ya está declarada")
 
   def add_function_params(self, func_name, param_type, param_name):
-    """ Adds parameters to the variables table of the corresponding function """
+    """ Add parameters to the variables table of the corresponding function """
     self.symbol_table["dir_functions"][func_name]["param_types"].append(param_type)
     self.symbol_table["dir_functions"][func_name]["variables"]['var_names'].add(param_name)
     self.symbol_table["dir_functions"][func_name]["variables"]['vars_info'].append({'name': param_name, 'type': param_type})
