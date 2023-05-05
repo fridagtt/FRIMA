d1f904837ced323acdd51d96675b186e941cdc84
Frida Gutiérrez <fridagtzm@gmail.com>
2023-05-05 11:32:50 -0600

    agregar direcciones

---
+++ utils/virtualMemory.py
@@ -0,0 +1,24 @@
+# memory for global variables
+global_int = 2000
+global_float = 4000
+global_char = 6000
+# memory for local variables
+local_int = 8000
+local_float = 10000
+local_char = 12000
+# memory for global temp variables
+global_temp_int = 14000
+global_temp_float = 16000
+global_temp_char = 18000
+global_temp_bool = 20000
+# memory for local temp variables
+local_temp_int = 22000
+local_temp_float = 24000
+local_temp_char = 26000
+local_temp_bool = 28000
+# memory for constant variables
+const_string = 30000
+const_int = 32000
+const_float = 34000
+const_char = 36000
+
